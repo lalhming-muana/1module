5074e886f37de76e09e2acbe38821cbde6e62a23
lhmuana <edd_vai@yahoo.co.in>
2025-11-15 21:14:43 +0530

    the material app and scaffold started

--- module1/lib/main.dart
+++ module1/lib/main.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      darkTheme: ThemeData.dark(),// this line
-      themeMode: ThemeMode.dark, // and this will give you darktheme mode
-      home: Scaffold(body: Center(child: Text('data start'))),
+      home: Scaffold(
+        backgroundColor: Colors.deepPurple,
+        appBar: AppBar(title: Text('Sample code'),),
+        body: Center(child: Text('Hello everyone'),),
+      ),
     ),
   );
 }
